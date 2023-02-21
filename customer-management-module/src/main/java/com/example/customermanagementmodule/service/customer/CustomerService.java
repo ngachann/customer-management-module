5740d2b0251a44cbdb2f7d7e626b65c36798c389
Hieu <hieud1161@gmail.com>
2023-02-21 19:07:08 +0700

    no message

--- customer-management-module/src/main/java/com/example/customermanagementmodule/service/customer/CustomerService.java
+++ customer-management-module/src/main/java/com/example/customermanagementmodule/service/customer/CustomerService.java
@@ -1,10 +1,11 @@
 package com.example.customermanagementmodule.service.customer;
 
 import com.example.customermanagementmodule.dto.Customers20To30AgeDTO;
+import com.example.customermanagementmodule.entity.Customer;
 
 import java.util.List;
 
 public interface CustomerService {
-
+    List<Customer> showListCustomerByListString(String str);
 
 }
--- customer-management-module/src/main/java/com/example/customermanagementmodule/service/customer/CustomerServiceImpl.java
+++ customer-management-module/src/main/java/com/example/customermanagementmodule/service/customer/CustomerServiceImpl.java
@@ -1,10 +1,21 @@
 package com.example.customermanagementmodule.service.customer;
 
+import com.example.customermanagementmodule.entity.Customer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
 
+    @Override
+    public List<Customer> showListCustomerByListString(String str) {
+//        "abc,abc,agh"
+                
+
+        return null;
+
+    }
 }
