2eab486cdc3f931229ceb7e8c2e4cf2e3698af73
Hieu <hieud1161@gmail.com>
2023-02-21 18:55:48 +0700

    add1

--- customer-management-module/src/main/java/com/example/customermanagementmodule/CustomerManagementModuleApplication.java
+++ customer-management-module/src/main/java/com/example/customermanagementmodule/CustomerManagementModuleApplication.java
@@ -9,6 +9,7 @@ public class CustomerManagementModuleApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerManagementModuleApplication.class, args);
         System.out.println(123);
+        System.out.println(123);
     }
 
 }
